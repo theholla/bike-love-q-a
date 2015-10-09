9916b90cc714e9e4775cba829020fa08ab4d6bcb
the holla <holla@bikefarm.org>
2015-10-09 01:12:14 -0700

    Fix update question route

---
+++ app/components/update-question.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        summary: this.get('summary'),
+        q: this.get('q'),
+        user: this.get('user'),
+        tags: this.get('tags'),
+        date: Date.now(),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    },
+
+    deleteQuestion(question) {
+      if(confirm('Delete this for reals?')) {
+        this.sendAction('deleteQuestion', question, params);
+      }
+    }
+  }
+});
--- tests/integration/components/update-question-test.js
+++ tests/integration/components/update-question-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('edit-question', 'Integration | Component | edit question', {
+moduleForComponent('update-question', 'Integration | Component | edit question', {
   integration: true
 });
 
@@ -11,15 +11,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{edit-question}}`);
+  this.render(hbs`{{update-question}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#edit-question}}
+    {{#update-question}}
       template block text
-    {{/edit-question}}
+    {{/update-question}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
