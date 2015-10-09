671d67e775077668e80548751c5eae18d4d6abe8
the holla <holla@bikefarm.org>
2015-10-08 21:53:55 -0700

    Add behavior for saving question and update JSON file

--- app/components/ask-question.js
+++ app/components/ask-question.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        summary: this.get('summary') ? this.get('summary') : "",
+        q: this.get('q') ? this.get('q') : "",
+        user: this.get('user') ? this.get('user') : "",
+        date: this.get('date') ? this.get('date') : "",
+        tags: this.get('tags') ? this.get('tags') : "",
+      };
+      this.sendAction('save', params);
+    }
+  }
 });
